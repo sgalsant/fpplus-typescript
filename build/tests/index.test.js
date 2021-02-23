f8facdf5813e544d66b06cd8085a88dded27cfab
sgalsant <sgalsant@gmail.com>
2021-02-23 21:16:31 +0000

    first commit

---
+++ build/tests/index.test.js
@@ -0,0 +1,8 @@
+"use strict";
+describe('calculate', function () {
+    it('add', function () {
+        let result = 7;
+        expect(result).toBe(7);
+    });
+});
+//# sourceMappingURL=index.test.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {Persona} from "./persona";
+
+let persona: Persona = new Persona();
+persona.nombre = "santiago";
+
+console.log("hola " + persona.nombre);
