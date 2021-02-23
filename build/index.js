89ebc11e1c89bf95d2cf6261c8ee4d9b57227503
sgalsant <sgalsant@gmail.com>
2021-02-23 21:21:22 +0000

    delete build

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const persona_1 = require("./persona");
-let persona = new persona_1.Persona();
-persona.nombre = "santiago";
-console.log("hola " + persona.nombre);
-//# sourceMappingURL=index.js.map
--- build/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-describe('calculate', function () {
-    it('add', function () {
-        let result = 7;
-        expect(result).toBe(7);
-    });
-});
-//# sourceMappingURL=index.test.js.map
