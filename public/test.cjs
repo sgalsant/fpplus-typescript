163e5a51ba916f1a554aad14f6b3ab51010a4163
sgalsant <sgalsant@gmail.com>
2021-02-26 21:41:14 +0000

    comentarios

--- public/test.cjs
+++ public/test.cjs
@@ -1,4 +1,4 @@
-// necesaario para ejecutar los test con mocha desde la consola
+// necesario para ejecutar los test con mocha desde la consola siendo equivalente cuando se ejecuta desde navegador
  
 global.chai = require('chai');
 global.mocha = require('mocha');
