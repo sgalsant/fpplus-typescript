2cac1724cf4597d758767f6522539dc375e8f382
sgalsant <sgalsant@gmail.com>
2021-02-26 22:25:02 +0000

    test

---
+++ test/mytest.test.ts
@@ -0,0 +1,11 @@
+//import {assert} from "chai";
+const assert = chai.assert; // usa public/test.cjs
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.strictEqual([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
